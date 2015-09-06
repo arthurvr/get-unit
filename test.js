fb1e3fc484d573768c3f951d20d540f87a521aa3
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 12:50:50 +0200

    Use `ava` for testing

--- test.js
+++ test.js
@@ -1,52 +1,52 @@
 'use strict';
+var test = require('ava');
 var getUnit = require('./');
-var assert = require('assert');
 
-it('Should be a function', function () {
-	assert.strictEqual(typeof getUnit, 'function');
+test('Should be a function', function (t) {
+	t.is(typeof getUnit, 'function');
 });
 
-it('Should return normal units', function () {
-	assert.equal(getUnit('5m'), 'm');
-	assert.equal(getUnit('300cm'), 'cm');
-	assert.equal(getUnit('2CM'), 'CM');
+test('Should return normal units', function (t) {
+	t.is(getUnit('5m'), 'm');
+	t.is(getUnit('300cm'), 'cm');
+	t.is(getUnit('2CM'), 'CM');
 });
 
-it('Should return the right unit when having a dot in the amount', function () {
-	assert.equal(getUnit('5.5m'), 'm');
-	assert.equal(getUnit('.4mm'), 'mm');
-	assert.equal(getUnit('3.3000009999009h'), 'h');
+test('Should return the right unit when having a dot in the amount', function (t) {
+	t.is(getUnit('5.5m'), 'm');
+	t.is(getUnit('.4mm'), 'mm');
+	t.is(getUnit('3.3000009999009h'), 'h');
 });
 
-it('Should return the right unit when having a comma in the amount', function () {
-	assert.equal(getUnit('5,5m'), 'm');
-	assert.equal(getUnit(',4mm'), 'mm');
-	assert.equal(getUnit('3,3000009999009h'), 'h');
+test('Should return the right unit when having a comma in the amount', function (t) {
+	t.is(getUnit('5,5m'), 'm');
+	t.is(getUnit(',4mm'), 'mm');
+	t.is(getUnit('3,3000009999009h'), 'h');
 });
 
-it('Should throw when passing something that isn\'t a string', function () {
+test('Should throw when passing something that isn\'t a string', function (t) {
 	[4, undefined, true, null, function () {}].forEach(function (val) {
-		assert.throws(function () {
+		t.throws(function () {
 			getUnit(val);
 		});
 	});
 });
 
-it('Should trim whitespace', function () {
+test('Should trim whitespace', function (t) {
 	['  5mm', '5mm   ', '  5mm  '].forEach(function (val) {
-		assert.strictEqual(getUnit(val), 'mm');
+		t.is(getUnit(val), 'mm');
 	});
 });
 
-it('Should return null when there is no unit', function () {
-	assert.strictEqual(getUnit('5'), null);
-	assert.strictEqual(getUnit('555'), null);
-	assert.strictEqual(getUnit('1.003'), null);
-	assert.strictEqual(getUnit('1,003'), null);
+test('Should return null when there is no unit', function (t) {
+	t.is(getUnit('5'), null);
+	t.is(getUnit('555'), null);
+	t.is(getUnit('1.003'), null);
+	t.is(getUnit('1,003'), null);
 });
 
-it('Should return a trimmed unit with a space between value and unit', function () {
-	assert.equal(getUnit('5,5 m'), 'm');
-	assert.equal(getUnit(',4 mm'), 'mm');
-	assert.equal(getUnit('3,3000009999009 h'), 'h');
+test('Should return a trimmed unit with a space between value and unit', function (t) {
+	t.is(getUnit('5,5 m'), 'm');
+	t.is(getUnit(',4 mm'), 'mm');
+	t.is(getUnit('3,3000009999009 h'), 'h');
 });
