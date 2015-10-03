d4221ca2e195c3094ad97702eed924dab8e2862e
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 11:43:30 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
-'use strict';
-var test = require('ava');
-var getUnit = require('./');
+import test from 'ava';
+import getUnit from './';
 
-test('Should be a function', function (t) {
+test('Should be a function', t => {
 	t.is(typeof getUnit, 'function');
 
 	t.end();
 });
 
-test('Should return normal units', function (t) {
+test('Should return normal units', t => {
 	t.is(getUnit('5m'), 'm');
 	t.is(getUnit('300cm'), 'cm');
 	t.is(getUnit('2CM'), 'CM');
@@ -16,7 +15,7 @@ test('Should return normal units', function (t) {
 	t.end();
 });
 
-test('Should return the right unit when having a dot in the amount', function (t) {
+test('Should return the right unit when having a dot in the amount', t => {
 	t.is(getUnit('5.5m'), 'm');
 	t.is(getUnit('.4mm'), 'mm');
 	t.is(getUnit('3.3000009999009h'), 'h');
@@ -24,7 +23,7 @@ test('Should return the right unit when having a dot in the amount', function (t
 	t.end();
 });
 
-test('Should return the right unit when having a comma in the amount', function (t) {
+test('Should return the right unit when having a comma in the amount', t => {
 	t.is(getUnit('5,5m'), 'm');
 	t.is(getUnit(',4mm'), 'mm');
 	t.is(getUnit('3,3000009999009h'), 'h');
@@ -32,9 +31,9 @@ test('Should return the right unit when having a comma in the amount', function 
 	t.end();
 });
 
-test('Should throw when passing something that isn\'t a string', function (t) {
-	[4, undefined, true, null, function () {}].forEach(function (val) {
-		t.throws(function () {
+test('Should throw when passing something that isn\'t a string', t => {
+	[4, undefined, true, null, function () {}].forEach(val => {
+		t.throws(() => {
 			getUnit(val);
 		}, TypeError);
 	});
@@ -42,15 +41,15 @@ test('Should throw when passing something that isn\'t a string', function (t) {
 	t.end();
 });
 
-test('Should trim whitespace', function (t) {
-	['  5mm', '5mm   ', '  5mm  '].forEach(function (val) {
+test('Should trim whitespace', t => {
+	['  5mm', '5mm   ', '  5mm  '].forEach(val => {
 		t.is(getUnit(val), 'mm');
 	});
 
 	t.end();
 });
 
-test('Should return null when there is no unit', function (t) {
+test('Should return null when there is no unit', t => {
 	t.is(getUnit('5'), null);
 	t.is(getUnit('555'), null);
 	t.is(getUnit('1.003'), null);
@@ -59,7 +58,7 @@ test('Should return null when there is no unit', function (t) {
 	t.end();
 });
 
-test('Should return a trimmed unit with a space between value and unit', function (t) {
+test('Should return a trimmed unit with a space between value and unit', t => {
 	t.is(getUnit('5,5 m'), 'm');
 	t.is(getUnit(',4 mm'), 'mm');
 	t.is(getUnit('3,3000009999009 h'), 'h');
